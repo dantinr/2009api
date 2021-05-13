e6146e1dd180fd70c16edc2e5fad6cd0766cf707
dante <dantinr@gmail.com>
2021-05-13 12:10:48 +0800

    用户列表

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mysql   = require('mysql');      //引入 mysql 类库
 const app = express()
 const port = 3000
+
+
+const connection = mysql.createConnection({
+    host     : 'localhost',         //数据库地址
+    user     : 'root',              //数据库用户名
+    password : '123456abc',            // 数据库密码
+    database : 'api2009'              // 数据库名
+});
+
+//建立连接
+connection.connect();
+
 
 app.get('/', (req, res) => {
 
@@ -15,11 +28,13 @@ app.get('/test',(req,res)=>{
 
 //用户列表接口
 app.get('/user/list',(req,res)=>{
+    //拼装sql语句
+    let sql = "select user_id,user_name,email,password from p_users order by user_id desc limit 10"
 
-
-    //返回json数据
+    connection.query(sql, function (error, results, fields) {
+        res.send(results)           //将数据库查询结果返回给接口
+    });
 })
-
 
 
 app.listen(port, () => {
